efddfd2ce0122890568f3a47ba3ddb562507a588
nassimb81 <nassimbiglari@gmail.com>
2018-10-12 11:10:53 +0200

    updating arduino

--- src/main/java/com/mt/nl/torchbot/HelloArduino.java
+++ src/main/java/com/mt/nl/torchbot/HelloArduino.java
@@ -3,35 +3,58 @@ package com.mt.nl.torchbot;
 import com.fazecast.jSerialComm.SerialPort;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 public class HelloArduino {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        SerialPort sp = SerialPort.getCommPort("/dev/ttyACM1");
-        sp.setComPortParameters(9600, 8, 1, 0); // default connection settings for Arduino
-        sp.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0); // block until bytes can be written
+        SerialPort[] ports = SerialPort.getCommPorts();
+        SerialPort port = null;
 
-        if (sp.openPort()) {
+        for (SerialPort p : ports) {
+            if (p.getSystemPortName().equals("/dev/COM3"))
+                ; /*for your question, <required_port> would be ttyACM0, but this can change if you reconnect the device, or if multiple devices are connected.*/
+            port = p;
+        }
+
+        port.setComPortParameters(9600, 8, 1, 0); // default connection settings for Arduino
+        port.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0); // block until bytes can be written
+
+        if (port.openPort()) {
             System.out.println("Port is open :)");
         } else {
             System.out.println("Failed to open port :(");
             return;
         }
 
-        for (Integer i = 0; i < 5; ++i) {
-            sp.getOutputStream().write(i.byteValue());
-            sp.getOutputStream().flush();
-            System.out.println("Sent number: " + i);
-            Thread.sleep(1000);
+        for (Integer i = 1; i < 10; ++i) {
+            InputStream inputStream;
+            System.out.println("Sending in byte number is: " + i);
+            port.getOutputStream().write(i.byteValue());
+            port.getOutputStream().flush();
+            System.out.println("Is it blinking?");
+            inputStream = port.getInputStream();
+
+            byte[] buffer= new byte[5];
+            int in = 1;
+            while (in < 5) {
+                buffer[in++] = (byte) inputStream.read();
+            }
+
+            System.out.println("Buffer is " + Arrays.toString(buffer));
+            String result = new String(buffer, "UTF8");
+            System.out.println("Buffer is " + result);
+
+            Thread.sleep(3000);
         }
 
-        if (sp.closePort()) {
+        if (port.closePort()) {
             System.out.println("Port is closed :)");
         } else {
             System.out.println("Failed to close port :(");
             return;
         }
-
 
     }
 }
